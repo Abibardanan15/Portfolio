e43380856248e8bde89a12495f203d9cc4b20095
Abibardanan15 <galaxynighters@gmail.com>
2025-12-03 10:19:10 +0700

    Add files via upload

---
+++ Portfolio 1 week in python/Portfolio number 2.py
@@ -0,0 +1,47 @@
+line01 = "****************************"
+line01 = "****************************"
+line02 = "*WELCOME TO ABIB CALCULATOR*"
+line03 = "****************************"
+line03 = "****************************"
+
+print(line01)
+print(line01)
+print(line02)
+print(line03)
+print(line03)
+print("")
+question1 = input("Welcome to Abib Calc how may i help you?: ")
+question2 = input("Sure just enter an operator and the number and you will be good: ")
+question3 = input("This is just the beginninf of Abib calc: ")
+print("He will build a amazing calc later: ")
+
+operator = input("Enter an Operator: ")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(f"The result is: {result}")
+elif operator == "-":
+    result = num1 - num2
+    print(f"The result is: {result}")
+elif operator == "*":
+    result = num1 * num2
+    print(f"The result is: {result}")
+elif operator == "/":
+    result = num1 / num2
+    print(f"The result is: {result}")
+
+print("")
+question3 = input("Thanks for using Abib Calc, Please leave a review: ")
+print("")
+line04 = "****************************"
+line04 = "****************************"
+line05 = "*THANKS FOR USING ABIB CALC*"
+line06 = "****************************"
+line06 = "****************************"
+print(line04)
+print(line04)
+print(line05)
+print(line06)
+print(line06)
